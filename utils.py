b9e77dd54e3ea3eea07e4c181832d88a3f7003f8
gabsito <gacagonz@espol.edu.ec>
2023-01-28 21:55:49 -0500

    cambiada la funcionalidad de la funcion readCSV, ahora retorna una matriz de numpy

--- utils.py
+++ utils.py
@@ -1,14 +1,15 @@
 import numpy as np
 
 
-def readCSV(file: str) -> dict:
-    rtn = {}
+def readCSV(file: str) -> np.array:
+    rtn = []
     with open(file, 'r') as f:
         lines = f.readlines()
     for line in lines:
         graph_id, points_str = line.strip().split(":")
         points = [tuple(map(int, point.replace('(', '').replace(')', '').split(','))) for point in
                   points_str.split(';')]
-        rtn[graph_id] = np.array(points)
+        rtn.append(points)
+    rtn = np.array(rtn)
     print(rtn)
     return rtn
