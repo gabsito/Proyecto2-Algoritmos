091ee59a691ac22d71d3bf2bcbbb155022555fac
aumoscos <andresulisesmoscoso@hotmail.com>
2023-01-28 22:29:22 -0500

    Lógica para comparar tramos

--- main.py
+++ main.py
@@ -5,4 +5,27 @@ import utils
 
 __FILE__ = "graficas.csv"
 
-graphs = utils.readCSV(__FILE__)
+matrix = utils.readCSV(__FILE__)
+#matrix = [[1, 2, 3], [4, 5, 6, 7], [8, 9]]
+n = len(matrix)
+m = max(len(row) for row in matrix)
+for row1 in range(n):
+    for col in range(m):
+        for row2 in range(row1+1, n):
+            if col+1 < len(matrix[row1]) and col+1 < len(matrix[row2]):
+                tramo1 = ""
+                tramo2 = ""
+                if matrix[row1][col][1] - matrix[row1][col+1][1] < 0:
+                    tramo1 = "creciente"
+                elif matrix[row1][col][1] - matrix[row1][col+1][1] > 0:
+                    tramo1 = "decreciente"
+                elif matrix[row1][col][1] - matrix[row1][col+1][1] == 0:
+                    tramo1 = "constante"
+                if matrix[row2][col][1] - matrix[row2][col+1][1] < 0:
+                    tramo2 = "creciente"
+                elif matrix[row2][col][1] - matrix[row2][col+1][1] > 0:
+                    tramo2 = "decreciente"
+                elif matrix[row2][col][1] - matrix[row2][col+1][1] == 0:
+                    tramo2 = "constante"
+                if tramo1 == tramo2:
+                    print("Tramo desde ", col , " a ", col+1, "iguales en :", row1, " y ", row2)
